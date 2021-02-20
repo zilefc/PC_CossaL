7023624abeaf23f16be10b7d1d61b35234c19346
zilefc <fczile@gmail.com>
2021-02-20 13:50:54 +0200

    Create PC_CossaLourenco.ino

---
+++ PC_CossaLourenco/PC_CossaLourenco.ino
@@ -0,0 +1,17 @@
+int sensorSala=3;
+int sensorCorredor=4;
+int lampadaSala=13;
+int lampadaCorredor=12;
+int ldrSala=5;
+int ldrCorredor=6;
+void setup() {
+  pinMode(lampadaSala,OUTPUT);
+  pinMode(lampadaCorredor,OUTPUT);
+  pinMode(ldrSala;INPUT);
+  pinMode(ldrCorredor,INPUT);
+  pinMode(sensorSala,INPUT);
+  pinMode(sensorCorredor,INPUT);
+}
+
+void loop() {
+}
