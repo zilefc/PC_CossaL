f1b71386a8563171cc214b7abfbde5aac99cac75
zilefc <fczile@gmail.com>
2021-02-20 14:56:51 +0200

    Update PC_CossaLourenco.ino

--- PC_CossaLourenco/PC_CossaLourenco.ino
+++ PC_CossaLourenco/PC_CossaLourenco.ino
@@ -16,34 +16,39 @@ void setup() {
   pinMode(ldrCorredor, INPUT);
   pinMode(sensorSala, INPUT);
   pinMode(sensorCorredor, INPUT);
-
+Serial.begin(9600);
   display.init();
   display.backlight();
-  display.setCursor(3, 0);
+  display.setCursor(0, 0);
   display.print("Iniciando..........");
-
+  display.setCursor(0, 1);
+  display.print("Cossa, Lourenco");
 }
 
 void loop() {
+   delay(1000);
+   display.clear();
   int LDRsala = analogRead(ldrSala);
   boolean SENSORsala = digitalRead(sensorSala);
   int LDRcorredor = analogRead(ldrCorredor);
   boolean SENSORcorredor = digitalRead(sensorCorredor);
+Serial.println("sala:");
+Serial.println(LDRsala);
+Serial.println(SENSORsala);
 
-  if (LDRsala > 500 && SENSORsala == LOW) {
+
+  if ((LDRsala <670) && SENSORsala == HIGH) {
     digitalWrite(lampadaSala, HIGH);
-    display.clear();
     display.setCursor(0, 0);
-    display.print("luz ligada");
+    display.print("luz Sl ligada");
   }
 
-  if (LDRcorredor > 500) {
+  if (LDRcorredor < 670) {
     analogWrite(lampadaCorredor, 127);
-    if ( SENSORcorredor == LOW) {
+    if ( SENSORcorredor == HIGH) {
       analogWrite(lampadaCorredor, 255);
-      display.clear();
-      display.setCursor(0, 0);
-      display.print("luz ligada");
+      display.setCursor(0, 1);
+      display.print("luz Cor ligada");
     }
   }
 }
